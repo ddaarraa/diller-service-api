305d7af5c532ae7e129295e2a1ddba323ec68879
Dara <dara@Daras-MacBook-Air.local>
2025-04-17 22:07:11 +0700

    implement Header to the api request

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
+from app.middleware.middleware import SecretKeyMiddleware
 from app.routes import raw_logs, correlation
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(SecretKeyMiddleware)
 app.include_router(raw_logs.router)
 app.include_router(correlation.router)
 
---
+++ app/middleware/middleware.py
@@ -0,0 +1,15 @@
+from fastapi import Request, HTTPException
+from starlette.middleware.base import BaseHTTPMiddleware
+
+SECRET_KEY = "w_RZQ0Gj1hMlEjUtAHXk3GnHRspRm8zKzPzE0xxm-Zs"
+
+class SecretKeyMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        public_paths = ["/docs", "/openapi.json", "/redoc"]
+        if request.url.path in public_paths:
+            return await call_next(request)
+        
+        key = request.headers.get("X-Secret-Key")
+        if key != SECRET_KEY:
+            raise HTTPException(status_code=403, detail="Forbidden: Invalid Secret Key")
+        return await call_next(request)
